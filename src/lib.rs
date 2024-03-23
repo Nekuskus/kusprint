559c0113a5a0cf111aa8d59983ab88291290ae11
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2024-03-23 15:03:09 +0100

    Add delim print macro.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
 #[macro_export]
 macro_rules! expr_print {
-    // The pattern for a single `eval`
     ($e:expr) => {
         {
             print!("{}", $e);
         }
     };
 
-    // Decompose multiple `eval`s recursively
     ($e:expr, $($es:expr),+) => {{
         expr_print! { $e }
         expr_print! { $($es),+ }
     }};
 }
+
+#[macro_export]
+macro_rules! expr_print_delim {
+    ($delim:expr, $e:expr, $($es:expr),+) => {{
+        expr_print! { $e }
+        expr_print! { $($delim, $es),+ }
+    }};
+}
+
