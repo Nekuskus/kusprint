ca019ed21d4d156565477ff15e65ba4dddd10e10
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2024-03-23 15:18:24 +0100

    Add macro for printing with line ending

--- src/lib.rs
+++ src/lib.rs
@@ -20,3 +20,27 @@ macro_rules! expr_print_delim {
     }};
 }
 
+#[macro_export]
+macro_rules! expr_println {
+    ($e:expr) => {
+        {
+            println!("{}", $e);
+        }
+    };
+
+    ($e:expr, $($es:expr),+) => {{
+        expr_print! { $e }
+        expr_print! { $($es),+ }
+        println!()
+    }};
+}
+
+#[macro_export]
+macro_rules! expr_println_delim {
+    ($delim:expr, $e:expr, $($es:expr),+) => {{
+        expr_print! { $e }
+        expr_print! { $($delim, $es),+ }
+        println!()
+    }};
+}
+
