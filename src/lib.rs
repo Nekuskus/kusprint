4b398cecabb7e28cece46f497b0a946ad6de6a1a
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2024-03-24 20:56:45 +0100

    Add label_print and variants

--- src/lib.rs
+++ src/lib.rs
@@ -44,3 +44,54 @@ macro_rules! expr_println_delim {
     }};
 }
 
+#[macro_export]
+macro_rules! label_print {
+    ($e:expr) => {
+        {
+            print!("{}: {}", String::from(stringify!($e)), $e);
+        }
+    };
+
+    ($e:expr, $($es:expr),+) => {{
+        label_print! { $e }
+        label_print! { $($es),+ }
+    }};
+}
+
+#[macro_export]
+macro_rules! label_print_delim {
+    ($delim:expr, $e:expr, $($es:expr),+) => {{
+        label_print! { $e }
+        $(
+            print!($delim);
+            label_print! { $es }
+        )*
+    }};
+}
+
+#[macro_export]
+macro_rules! label_println {
+    ($e:expr) => {
+        {
+            println!("{}", $e);
+        }
+    };
+
+    ($e:expr, $($es:expr),+) => {{
+        label_println! { $e }
+        label_println! { $($es),+ }
+        println!()
+    }};
+}
+
+#[macro_export]
+macro_rules! label_println_delim {
+    ($delim:expr, $e:expr, $($es:expr),+) => {{
+        label_println! { $e }
+        $(
+            print!($delim);
+            label_println! { $es }
+        )*
+    }};
+}
+
